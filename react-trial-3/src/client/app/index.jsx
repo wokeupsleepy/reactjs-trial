9372914f950b285643b531986a1829a58987c71d
wokeupsleepy <wokeupsleepy@gmail.com>
2016-11-28 10:33:35 -0500

    initial commit, basic explanation of ReactJS elements, components, props, and ReactDOM.render()

---
+++ react-trial-3/src/client/app/index.jsx
@@ -0,0 +1,75 @@
+﻿import React from 'react';
+import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+
+/*
+NOTE: Most of the steps to get to this point come from here:
+https://www.codementor.io/tamizhvendan/tutorials/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack-du107r9zr
+In Setting Up Babel-Loader section, it is needed to install babel-core
+(command in Command Prompt: npm install --save-dev babel-core).
+
+Remember to navigate to the local website folder before running npm commands in Command Prompt
+Remember to run Command Prompt as Admin
+
+The following command:
+node_modules\.bin\webpack -d --watch
+is run from the local website folder in CLI (Admin) in order to constantly monitor and convert the index.jsx.
+
+To remove/fix the syntax highlighting/syntax error detection, follow Solution 2 from the first reply here:
+http://stackoverflow.com/questions/34097915/visual-studio-2015-jsx-es2015-syntax-highlighting
+*/
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Donner',
+    lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+}
+
+const secondElement = getGreeting(user);
+
+//ReactDOM.render(secondElement, document.getElementById('app'));
+
+//NOTE: Renders a clock of the current time
+function tick() {
+    //NOTE: "Welcome(input)" is a ReactJS component, "input" is a ReactJS props (input for a ReactJS component)
+    function Welcome(input) {
+        return <h3>Hello, {input.name}!</h3>;
+    }
+
+    //NOTE: That the Welcome tag below is rendered using the above Welcome() function
+    //NOTE: "element" is a ReactJS element, it's essentially a piece of "renderable" HTML
+    const element = (
+      <div>
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
+          <Welcome name="Sara" />
+      </div>
+    );
+
+    ReactDOM.render(
+      element,
+      document.getElementById('clock')
+    );
+}
+
+//NOTE: Updates the clock every second with the ReactDOM.render() call inside tick()
+setInterval(tick, 1000);
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
---
+++ react-trial-3/webpack.config.js
@@ -0,0 +1,32 @@
+﻿var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    //entry: path.resolve(APP_DIR, '/index.jsx'),
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+          {
+              test: /\.jsx?/,
+              include: APP_DIR,
+              loader: 'babel'
+          }
+        ]
+        //loaders: [
+        //  {
+        //      test: /\.tsx?/,
+        //      include: APP_DIR,
+        //      loader: 'babel'
+        //  }
+        //]
+    }
+};
+
+module.exports = config;
